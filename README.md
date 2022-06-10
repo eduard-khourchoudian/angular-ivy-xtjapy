14aec29c8684a165b67142c320ad2784f9b5cb59
eduard-khourchoudian <eduard_khourchoudian@hotmail.com>
2022-06-09 23:06:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-xtjapy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-xtjapy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'User Registiration';
+  public userDOB: number;
+  public userDOBAge: string;
+  public userAge: string;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+
+  onDboChange(userDOB: Date) {
+    this.userDOBAge = '';
+    const bdate = new Date(userDOB);
+    let timeDiff = Math.abs(Date.now() - bdate.getTime());
+    let age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+
+    alert('DBO : ' + userDOB + ' age : ' + age);
+    if (age < 21) {
+      this.userDOBAge = 'User is younger than 21 years old';
+    }
+  }
+
+  onSubmit(data) {
+    console.log(data);
+  }
+}
